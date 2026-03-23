5a764e23ff92ead49539f608c230a7ecf51afdd8
Malik Ahsan <malikahsan13@gmail.com>
2026-03-23 15:09:59 +0500

    added single post and all posts endpoints

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+text_posts = {1: {"title": "New Post", "content": "Coll test post"}}
+
+
+@app.get("/posts")
+def get_all_posts():
+    return text_posts
+
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    if id not in text_posts:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return text_posts.get(id)
