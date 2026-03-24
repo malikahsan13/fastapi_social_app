b62fba395ba0cff9b02d00f13e9efc65b4e45a3e
Malik Ahsan <malikahsan13@gmail.com>
2026-03-25 01:05:56 +0500

    added get feed method

--- app/app.py
+++ app/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
 from app.schema import PostCreate, PostResponse
-from app.db import Post, create_db_and_tables, get_sync_session
+from app.db import Post, create_db_and_tables, get_async_session
 from app.db import Post
 from sqlalchemy.ext.asyncio import AsyncSession
 from contextlib import asynccontextmanager
+from sqlalchemy import select
 
 
 @asynccontextmanager
@@ -15,7 +16,7 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.post("/upload")
-async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_sync_session)):
+async def upload_file(file: UploadFile = File(...), caption: str = Form(""), session: AsyncSession = Depends(get_async_session)):
     post = Post(
         caption=caption,
         url="dummy url",
@@ -26,3 +27,22 @@ async def upload_file(file: UploadFile = File(...), caption: str = Form(""), ses
     await session.commit()
     await session.refresh(post)
     return post
+
+
+@app.get("/feed")
+async def get_feed(session: AsyncSession = Depends(get_async_session)):
+    result = await session.execute(select(Post).order_by(Post.created_at.desc()))
+    posts = [row[0] for row in result.all()]
+
+    posts_data = []
+    for post in posts:
+        posts_data.append(
+            {
+                "id": str(post.id),
+                "caption": post.caption,
+                "url": post.url,
+                "file_type": post.file_type,
+                "file_name": post.file_name,
+                "created_at": post.created_at.isoformat()
+            }
+        )
