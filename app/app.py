7872a46c4ee65af6fb02b8070f4440e349f2f62b
Malik Ahsan <malikahsan13@gmail.com>
2026-03-24 23:50:28 +0500

    added life span method

--- app/app.py
+++ app/app.py
@@ -1,6 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from app.schema import PostCreate, PostResponse
+from app.db import Post, create_db_and_tables, get_sync_session
+from app.db import post
+from sqlalchemy.ext.asyncio import AsyncSession
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await create_db_and_tables()
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 text_posts = {
     1: {"title": "New Post", "content": "Coll test post"},
@@ -24,7 +35,15 @@ def get_all_posts(limit: int = None):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int) -> PostResponse:
     if id not in text_posts:
         raise HTTPException(status_code=404, detail="Post not found")
     return text_posts.get(id)
+
+
+@app.post("/posts")
+def create_post(post: PostCreate) -> PostResponse:
+    new_post = {"title": post.title, "content": post.content}
+    text_posts[max(text_posts.keys()) +
+               1] = new_post
+    return new_post
