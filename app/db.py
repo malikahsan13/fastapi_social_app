aa718f2c44126b4021f35495e5dcef6bd82a3ed6
Malik Ahsan <malikahsan13@gmail.com>
2026-03-24 23:56:14 +0500

    fixed issue with declarative base

--- app/db.py
+++ app/db.py
@@ -31,7 +31,7 @@ async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 
 async def get_sync_session() -> AsyncGenerator[AsyncSession, None]:
