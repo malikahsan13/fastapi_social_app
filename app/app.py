0d0d13007aec9e5854df9ed7365f67d1d89db27e
Malik Ahsan <malikahsan13@gmail.com>
2026-03-27 01:07:23 +0500

    connected jwt logic with app and added routes

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, HTTPException, File, UploadFile, Form, Depends
-from app.schema import PostCreate, PostResponse
+from app.schema import PostCreate, PostResponse, UserRead, UserCreate, UserUpdate
 from app.db import Post, create_db_and_tables, get_async_session
 from app.db import Post
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -11,6 +11,7 @@ import shutil
 import os
 import uuid
 import tempfile
+from app.users import auth_backend, current_active_user, fastapi_users
 
 
 @asynccontextmanager
@@ -19,6 +20,17 @@ async def lifespan(app: FastAPI):
     yield
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(fastapi_users.get_auth_router(
+    auth_backend), prefix='/auth/jwt', tags=["auth"])
+app.include_router(fastapi_users.get_register_router(
+    UserRead, UserCreate), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_reset_password_router(),
+                   prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_verify_router(
+    UserRead), prefix="/auth", tags=["auth"])
+app.include_router(fastapi_users.get_users_router(
+    UserRead, UserUpdate), prefix="/users", tags=["users"])
 
 
 @app.post("/upload")
