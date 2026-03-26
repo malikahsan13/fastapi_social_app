f16739711525bfca056dfca9961d32d88d13823d
Malik Ahsan <malikahsan13@gmail.com>
2026-03-27 00:55:55 +0500

    added complete logic for jwt token

--- app/users.py
+++ app/users.py
@@ -9,3 +9,47 @@ from fastapi_users.authentication import (
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
 from app.db import User, get_user_db
+
+SECRET = "123456"
+
+
+class UserManager(UUIDIDMixin, BaseUserManager[User, uuid.UUID]):
+    reset_password_token_secret = SECRET
+    verification_token_secret = SECRET
+
+    async def on_after_register(self, user, request=None):
+        # return await super().on_after_register(user, request)
+        print(f"User {user.id} has registered.")
+
+    async def on_after_request_verify(self, user, token, request=None):
+        # return await super().on_after_request_verify(user, token, request)
+        print(
+            f"Verification requested for User {user.id}. Verification token: {token}")
+
+    async def on_after_forgot_password(self, user, token, request=None):
+        # return await super().on_after_forgot_password(user, token, request)
+        print(f"User {user.id} has forgot their password. Resest token: {token}")
+
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
+
+bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+
+
+def get_jwt_strategy():
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=bearer_transport,
+    get_strategy=get_jwt_strategy
+)
+
+
+fastapi_users = FastAPIUsers[User, uuid.UUID](
+    get_user_manager, auth_backends=[auth_backend])
+
+
+current_active_user = fastapi_users.current_user(active=True)
